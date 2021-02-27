cc182dfe4fb69b0d578268bf9c734525f9ddd006
knzkhuka <one235813@gmail.com>
2021-02-27 20:41:25 +0900

    lib add mulit precision integer

--- scripts/code_extension.sh
+++ scripts/code_extension.sh
@@ -1,6 +1,6 @@
 libpath="/mnt/c/Users/knzk/Documents/competitive_programming/library/"
-keywords=("rep" "repr" "i64" "pii" "pll" "i128" "inf" "INF" "newl" "pi" "eps" "vec<" "vec" "pii" "pll" "_i" "dout" "ceil" "chmax" "chmin" "divisor" "prime_factorization" "dxdy" "mpow" "minv" "1000000007" "998244353" "segtree" "union_find" "comb")
-snippet=("rep.cpp" "repr.cpp" "i64.cpp" "pii.cpp" "pll.cpp" "i128.cpp" "inf.cpp" "infl.cpp" "newl.cpp" "pi.cpp" "eps.cpp" "make_vector.cpp" "io_vector.cpp" "io_pair.cpp" "io_pair.cpp" "operator_i.cpp" "dout.cpp" "ceil.cpp" "chmax.cpp" "chmin.cpp" "divisor.cpp" "prime_factorization.cpp" "dxdy.cpp" "mpow.cpp" "minv.cpp" "modint.cpp" "modint998244353.cpp" "segment_tree.cpp" "union_find.cpp" "combination.cpp")
+keywords=("rep" "repr" "i64" "pii" "pll" "i128" "bigint" "inf" "INF" "newl" "pi" "eps" "vec<" "vec" "pii" "pll" "_i" "dout" "ceil" "chmax" "chmin" "divisor" "prime_factorization" "dxdy" "mpow" "minv" "1000000007" "998244353" "segtree" "union_find" "comb")
+snippet=("rep.cpp" "repr.cpp" "i64.cpp" "pii.cpp" "pll.cpp" "i128.cpp" "bigint.cpp" "inf.cpp" "infl.cpp" "newl.cpp" "pi.cpp" "eps.cpp" "make_vector.cpp" "io_vector.cpp" "io_pair.cpp" "io_pair.cpp" "operator_i.cpp" "dout.cpp" "ceil.cpp" "chmax.cpp" "chmin.cpp" "divisor.cpp" "prime_factorization.cpp" "dxdy.cpp" "mpow.cpp" "minv.cpp" "modint.cpp" "modint998244353.cpp" "segment_tree.cpp" "union_find.cpp" "combination.cpp")
 
 cat $libpath"head.cpp" > submit.cpp
 
