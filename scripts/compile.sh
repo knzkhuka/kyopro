7bf38abeb76580f843796ec3ca1d1ae09f28edc9
knzkhuka <one235813@gmail.com>
2021-03-24 19:38:07 +0900

    update lib

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,5 @@
 /mnt/c/Users/knzk/Documents/competitive_programming/scripts/code_extension.sh $1
 cat submit.cpp | clip.exe
 echo "extensioned."
-g++ -std=c++17 -O2 -o out -D_local -D_GLIBCXX_DEBUG submit.cpp
+# g++ -std=c++17 -O2 -o out -D_local -D_GLIBCXX_DEBUG submit.cpp
+g++ -std=c++17 -O2 -o out -D_local submit.cpp
